d4ef9e7ab3f248dce18eb3d7d4d29b7688f0891b
llepore <llepore@tesla.com>
2021-05-17 08:48:51 -0700

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"art/sketch"
 	"image"
 	"image/png"
 	"log"
 	"math/rand"
-	"net/http"
 	"os"
 	"time"
 
-	"github.com/preslavrachev/generative-art-in-go/sketch"
+	"github.com/fogleman/gg"
 )
 
 var (
-	sourceImgName   = "source.jpg"
-	outputImgName   = "out.png"
-	totalCycleCount = 5000
+	srcImageName = "srcImage3.jpg"
+	outImgName   = "outImage3.png"
+	cycleCount   = 5000
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
 
-	img, err := loadImage(sourceImgName)
-	/*
-		use this for completely random results
-		img, err := loadRandomUnsplashImage(2000, 2000)
-	*/
+	srcImg, err := gg.LoadImage(srcImageName)
 	if err != nil {
 		log.Panicln(err)
 	}
 
 	destWidth := 2000
-	s := sketch.NewSketch(img, sketch.UserParams{
+	sketch := sketch.NewSketch(srcImg, sketch.UserParams{
+
 		StrokeRatio:              0.75,
 		DestWidth:                destWidth,
 		DestHeight:               2000,
@@ -43,33 +40,12 @@ func main() {
 		MinEdgeCount:             3,
 		MaxEdgeCount:             4,
 	})
-
-	rand.Seed(time.Now().Unix())
-
-	for i := 0; i < totalCycleCount; i++ {
-		s.Update()
+	for i := 0; i < cycleCount; i++ {
+		sketch.Update()
 	}
 
-	saveOutput(s.Output(), outputImgName)
-}
+	saveOutput(sketch.Output(), outImgName)
 
-func loadRandomUnsplashImage(width, height int) (image.Image, error) {
-	url := fmt.Sprintf("https://source.unsplash.com/random/%dx%d", width, height)
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	img, _, err := image.Decode(res.Body)
-	return img, err
-}
-
-func loadImage(src string) (image.Image, error) {
-	file, _ := os.Open(sourceImgName)
-	defer file.Close()
-	img, _, err := image.Decode(file)
-	return img, err
 }
 
 func saveOutput(img image.Image, filePath string) error {
--- sketch/sketch.go
+++ sketch/sketch.go
@@ -9,22 +9,22 @@ import (
 )
 
 type UserParams struct {
-	StrokeRatio              float64
 	DestWidth                int
 	DestHeight               int
-	InitialAlpha             float64
-	StrokeReduction          float64
-	AlphaIncrease            float64
-	StrokeInversionThreshold float64
 	StrokeJitter             int
 	MinEdgeCount             int
 	MaxEdgeCount             int
+	StrokeRatio              float64
+	StrokeReduction          float64
+	StrokeInversionThreshold float64
+	InitialAlpha             float64
+	AlphaIncrease            float64
 }
 
 type Sketch struct {
-	UserParams        // embed for easier access
+	params            UserParams
 	source            image.Image
-	dc                *gg.Context
+	dc                *gg.Context // drawing context
 	sourceWidth       int
 	sourceHeight      int
 	strokeSize        float64
@@ -32,15 +32,15 @@ type Sketch struct {
 }
 
 func NewSketch(source image.Image, userParams UserParams) *Sketch {
-	s := &Sketch{UserParams: userParams}
+	s := &Sketch{params: userParams}
 	bounds := source.Bounds()
 	s.sourceWidth, s.sourceHeight = bounds.Max.X, bounds.Max.Y
-	s.initialStrokeSize = s.StrokeRatio * float64(s.DestWidth)
+	s.initialStrokeSize = s.params.StrokeRatio * float64(s.params.DestWidth)
 	s.strokeSize = s.initialStrokeSize
 
-	canvas := gg.NewContext(s.DestWidth, s.DestHeight)
+	canvas := gg.NewContext(s.params.DestWidth, s.params.DestHeight)
 	canvas.SetColor(color.Black)
-	canvas.DrawRectangle(0, 0, float64(s.DestWidth), float64(s.DestHeight))
+	canvas.DrawRectangle(0, 0, float64(s.params.DestWidth), float64(s.params.DestHeight))
 	canvas.FillPreserve()
 
 	s.source = source
@@ -49,35 +49,37 @@ func NewSketch(source image.Image, userParams UserParams) *Sketch {
 }
 
 func (s *Sketch) Update() {
+	// core drawing logic
+	// 1. Obtain color information from the source
 	rndX := rand.Float64() * float64(s.sourceWidth)
 	rndY := rand.Float64() * float64(s.sourceHeight)
 	r, g, b := rgb255(s.source.At(int(rndX), int(rndY)))
 
-	destX := rndX * float64(s.DestWidth) / float64(s.sourceWidth)
-	destX += float64(randRange(s.StrokeJitter))
-	destY := rndY * float64(s.DestHeight) / float64(s.sourceHeight)
-	destY += float64(randRange(s.StrokeJitter))
-	edges := s.MinEdgeCount + rand.Intn(s.MaxEdgeCount-s.MinEdgeCount+1)
+	// 2. Determine a destination in the output space
+	destX := rndX * float64(s.params.DestWidth) / float64(s.sourceWidth)
+	destX += float64(randRange(s.params.StrokeJitter))
+	destY := rndY * float64(s.params.DestHeight) / float64(s.sourceHeight)
+	destY += float64(randRange(s.params.StrokeJitter))
 
-	s.dc.SetRGBA255(r, g, b, int(s.InitialAlpha))
+	// 3. Draw a "stroke" using the desired parameters
+	edges := s.params.MinEdgeCount + rand.Intn(s.params.MaxEdgeCount-s.params.MinEdgeCount+1)
+
+	s.dc.SetRGBA255(r, g, b, int(s.params.InitialAlpha))
 	s.dc.DrawRegularPolygon(edges, destX, destY, s.strokeSize, rand.Float64())
 	s.dc.FillPreserve()
 
-	if s.strokeSize <= s.StrokeInversionThreshold*s.initialStrokeSize {
+	if s.strokeSize <= s.params.StrokeInversionThreshold*s.initialStrokeSize {
 		if (r+g+b)/3 < 128 {
-			s.dc.SetRGBA255(255, 255, 255, int(s.InitialAlpha*2))
+			s.dc.SetRGBA255(255, 255, 255, int(s.params.InitialAlpha*2))
 		} else {
-			s.dc.SetRGBA255(0, 0, 0, int(s.InitialAlpha*2))
+			s.dc.SetRGBA255(0, 0, 0, int(s.params.InitialAlpha*2))
 		}
 	}
 	s.dc.Stroke()
 
-	s.strokeSize -= s.StrokeReduction * s.strokeSize
-	s.InitialAlpha += s.AlphaIncrease
-}
-
-func (s *Sketch) Output() image.Image {
-	return s.dc.Image()
+	// 4. Update the parameter state for the next execution
+	s.strokeSize -= s.params.StrokeReduction * s.strokeSize
+	s.params.InitialAlpha += s.params.AlphaIncrease
 }
 
 func rgb255(c color.Color) (r, g, b int) {
@@ -87,4 +89,8 @@ func rgb255(c color.Color) (r, g, b int) {
 
 func randRange(max int) int {
 	return -max + rand.Intn(2*max)
+}
+
+func (s *Sketch) Output() image.Image {
+	return s.dc.Image()
 }
